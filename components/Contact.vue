2045c8288245334c86091fbdb1a6667c25b46941
HaileabT <haileabtesfaye11@gmail.com>
2024-11-02 10:12:34 -0400

    finished contact component with all its functionality

--- components/Contact.vue
+++ components/Contact.vue
@@ -25,32 +25,63 @@
         </ul>
       </div>
       <form
-        class="flex flex-col gap-3 bg-dark-bg px-4 py-6 rounded-[0.5rem]"
-        @submit.prevent=""
+        class="relative overflow-hidden flex flex-col gap-3 bg-dark-bg px-4 py-6 rounded-[0.5rem]"
+        @submit.prevent="sendContactEmail"
       >
-        <Input
+        <div
+          v-if="sendingEmail"
+          class="absolute top-0 left-0 w-full h-full backdrop-blur-[5px] bg-[#10101067] grid place-items-center"
+        >
+          <h3 class="text-dark-sub-header-text font-bold text-xl">Sending</h3>
+        </div>
+        <div
+          v-if="hasMailError"
+          class="absolute top-0 left-0 w-full h-full backdrop-blur-[5px] bg-[#101010b7] grid place-items-center"
+        >
+          <div class="text-dark-sub-header-text font-bold text-xl relative">
+            <i
+              class="fa-solid fa-circle-exclamation text-[#ff5353] block text-center mb-4 relative animate-slide-down"
+            ></i>
+            <h4 class="relative animate-slide-up">
+              {{ mailingErrorMessage }}
+            </h4>
+          </div>
+        </div>
+        <div
+          v-if="hasMailSuccess"
+          class="absolute top-0 left-0 w-full h-full backdrop-blur-[5px] bg-[#101010b7] grid place-items-center"
+        >
+          <div class="text-dark-sub-header-text font-bold text-xl relative">
+            <i
+              class="fa-solid fa-check text-dark-header-text block text-center mb-4 relative animate-slide-down"
+            ></i>
+            <h4 class="relative animate-slide-up">Thanks for reaching out.</h4>
+          </div>
+        </div>
+        <input
           v-model="name"
           type="text"
           pattern="[A-Za-z ]+"
           placeholder="Full Name"
           class="block w-full px-2 py-2 text-lg bg-transparent border-solid border-[1px] border-dark-header-text rounded-[0.2rem] text-dark-normal-text focus:outline-none placeholder:text-dark-normal-text"
         />
-        <Input
+        <input
           v-model="email"
           type="email"
           placeholder="Email"
           class="block w-full px-2 py-2 text-lg bg-transparent border-solid border-[1px] border-dark-header-text rounded-[0.2rem] text-dark-normal-text focus:outline-none placeholder:text-dark-normal-text"
         />
-        <Textarea
+        <textarea
           v-model="message"
           placeholder="Your message"
           class="block w-full px-2 py-2 text-lg bg-transparent border-solid border-[1px] border-dark-header-text rounded-[0.2rem] text-dark-normal-text focus:outline-none placeholder:text-dark-normal-text"
-        ></Textarea>
-        <Input:button
-          @click.prevent=""
+        ></textarea>
+        <input
+          type="submit"
+          value="Submit"
+          @click.prevent="sendContactEmail"
           class="pointer-grow cursor-pointer block w-max px-6 py-2 text-lg bg-dark-header-text rounded-[0.2rem] text-dark-bg font-extrabold focus:outline-none placeholder:text-dark-normal-text"
-          >Submit</Input:button
-        >
+        />
       </form>
     </div>
   </div>
@@ -59,46 +90,80 @@
 <script setup lang="ts">
 import { z } from "zod";
 
-const name = ref<string>("");
-const email = ref<string>("");
-const message = ref<string>("");
+const name = ref("");
+const email = ref("");
+const message = ref("");
+const sendingEmail = ref<boolean>(false);
+const hasMailError = ref<boolean>(false);
+const hasMailSuccess = ref<boolean>(false);
+const mailingErrorMessage = ref<string>("");
 
-// const mailer = useMail();
+const resetMailingStatus = (sucess: boolean) => {
+  if (sucess) {
+    hasMailSuccess.value = false;
+  } else {
+    hasMailError.value = false;
+  }
+};
+
 const { socials } = useSocials();
 
-// const contactSchema = z.object({
-//   name: z.string().trim().min(1, "Name is too short."),
-//   email: z.string().email("Invalid Email."),
-//   message: z.string().trim().min(1, "Message is too short."),
-// });
+const contactSchema = z.object({
+  name: z.string().trim().min(1, "Name is too short."),
+  email: z.string().email("Invalid Email."),
+  message: z.string().trim().min(1, "Message is too short."),
+});
 
-// contactSchema.required({
-//   name: true,
-//   email: true,
-//   message: true,
-// });
+contactSchema.required({
+  name: true,
+  email: true,
+  message: true,
+});
 
-// const sendContactEmail = async () => {
-//   const formDataParsed = await contactSchema.safeParseAsync({
-//     name: name.value,
-//     email: email.value,
-//     message: message.value,
-//   });
+const sendContactEmail = async () => {
+  const formDataParsed = await contactSchema.safeParseAsync({
+    name: name.value,
+    email: email.value,
+    message: message.value,
+  });
 
-//   if (formDataParsed.success) {
-//     mailer.send({
-//       from: name.value + " from " + email.value,
-//       to: process.env.EMAIL_USER,
-//       subject:
-//         "From " +
-//         email.value +
-//         ", email " +
-//         email.value +
-//         ", I want to work with your company ",
-//       text: message.value,
-//     });
-//   }
-// };
+  console.log(formDataParsed);
+  if (!formDataParsed.success) {
+    hasMailError.value = true;
+    mailingErrorMessage.value = formDataParsed.error.errors[0].message;
+    return setTimeout(resetMailingStatus.bind(null, false), 2000);
+  }
+  // console.log({ name: name.value, email: email.value });
+
+  if (formDataParsed.success) {
+    try {
+      sendingEmail.value = true;
+      const response = await $fetch("/api/sendMail", {
+        method: "POST",
+        body: {
+          from: name.value + " from " + email.value,
+          subject:
+            "From " +
+            email.value +
+            ", email " +
+            email.value +
+            ", I want to work with your company ",
+          text: message.value,
+        },
+      });
+      hasMailSuccess.value = true;
+      mailingErrorMessage.value = "Something went wrong.";
+      setTimeout(resetMailingStatus.bind(null, true), 2000);
+      // console.log(response);
+    } catch (err: any) {
+      hasMailError.value = true;
+      mailingErrorMessage.value = "Something went wrong.";
+      setTimeout(resetMailingStatus.bind(null, false), 2000);
+    } finally {
+      sendingEmail.value = false;
+    }
+  }
+};
 </script>
 
 <style scoped>
