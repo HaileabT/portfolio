00c12c1bf4f5f3645ff08dc9a4570122f77d4e86
HaileabT <haileabtesfaye11@gmail.com>
2026-01-03 01:52:56 -0500

    ref: cleaned up index page and added meta details on the app.vue

--- app/app.vue
+++ app/app.vue
@@ -5,6 +5,44 @@
     </div>
 </template>
 
-<script setup>
-// import { VueReCaptcha } from "vue-recaptcha-v3";
+<script setup lang="ts">
+useHead({
+    title: "Haileab Tesfaye",
+    htmlAttrs: {
+        lang: "en-us",
+    },
+    meta: [
+        {
+            name: "keywords",
+            content:
+                "Haileab Tesfaye, Portfolio, Website, Nuxtjs, Nuxt, Vue, GSAP, Lenis, Nodejs, Go, Typescript, JavaScript, HTML, CSS",
+        },
+        {
+            name: "author",
+            content: "Haileab Tesfaye",
+        },
+        {
+            name: "description",
+            content: "I'm Haileab Tesfaye; A Backend Developer.",
+        },
+    ],
+});
+
+useSeoMeta({
+    ogTitle: "Haileab Tesfaye",
+    ogImage: "/preview.jpg",
+    ogImageAlt: "Haileab Tesfaye",
+    ogSiteName: "Haileab Tesfaye",
+    ogDescription: "I'm Haileab Tesfaye; A Backend Developer.",
+    twitterTitle: "Haileab Tesfaye",
+    twitterImage: "/preview.jpg",
+    twitterImageAlt: "Haileab Tesfaye",
+    twitterSite: "@haileabtesfaye",
+    twitterCreator: "@haileabtesfaye",
+    twitterCard: "summary_large_image",
+    author: "Haileab Tesfaye",
+    description: "I'm Haileab Tesfaye; A Backend Developer.",
+    charset: "utf-8",
+    viewport: "width=device-width, initial-scale=1",
+});
 </script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -80,49 +80,6 @@ const logoContent = ref();
 
 const { socials } = useSocials();
 
-useHead({
-    title: "Haileab Tesfaye",
-    htmlAttrs: {
-        lang: "en-us",
-    },
-    meta: [
-        {
-            name: "keywords",
-            content:
-                "Haileab Tesfaye, Portfolio, Website, Nuxtjs, Nuxt, Vue, GSAP, Lenis, Nodejs, Go, Typescript, JavaScript, HTML, CSS",
-        },
-        {
-            name: "author",
-            content: "Haileab Tesfaye",
-        },
-        {
-            name: "description",
-            content: "I'm Haileab Tesfaye; A Backend Developer.",
-        },
-    ],
-});
-
-useSeoMeta({
-    ogTitle: "Haileab Tesfaye",
-    ogImage: "/preview.jpg",
-    ogImageAlt: "Haileab Tesfaye",
-    ogSiteName: "Haileab Tesfaye",
-    ogDescription: "I'm Haileab Tesfaye; A Backend Developer.",
-    twitterTitle: "Haileab Tesfaye",
-    twitterImage: "/preview.jpg",
-    twitterImageAlt: "Haileab Tesfaye",
-    twitterSite: "@haileabtesfaye",
-    twitterCreator: "@haileabtesfaye",
-    twitterCard: "summary_large_image",
-    author: "Haileab Tesfaye",
-    description: "I'm Haileab Tesfaye; A Backend Developer.",
-    charset: "utf-8",
-    viewport: "width=device-width, initial-scale=1",
-});
-
-const hRef = ref(null);
-// console.log(useRuntimeConfig().logo_cloud_url);
-
 onMounted(async () => {
     if (useRuntimeConfig().public.logo_cloud_url) {
         logoContent.value = await $fetch(
